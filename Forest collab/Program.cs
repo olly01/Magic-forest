ef0ccd2d4752eaac262ffdc5536ba888abc73236
Alcina Thomas Emili <17U-AThomas@utccambridge.co.uk>
2020-12-09 10:30:05 +0000

    class of tree

--- Forest collab/Program.cs
+++ Forest collab/Program.cs
@@ -7,7 +7,6 @@ namespace Forest_collab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("ciao");
         }
     }
 }
