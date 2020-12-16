6a518ab6db1daa8eb136a4fef6832232204a758d
Annabelle Parry <17U-AParry@utccambridge.co.uk>
2020-12-16 10:28:12 +0000

    styidfug

--- Forest collab/Program.cs
+++ Forest collab/Program.cs
@@ -12,15 +12,13 @@ namespace Forest_collab
             int[] RiverDisplay = new int[] { 79, 80, 81, 82, 180, 181, 182, 183, 282, 283, 284, 285, 383, 384, 385, 386, 482, 483, 484, 485, 581, 582, 583, 584, 680, 681, 682, 683, 779, 780, 781, 878, 879, 880, 978, 979, 980, 1079, 1080, 1081, 1082, 1180, 1181, 1182, 1183, 1184, 1281, 1282, 1283, 1284, 1285, 1286, 1382, 1383, 1387, 1388, 1484, 1485, 1486, 1487, 1488, 1489, 1490, 1586, 1587, 1588, 1589, 1590, 1591, 1592, 1689, 1690, 1691, 1692, 1693, 1694, 1791, 1792, 1793, 1794, 1795, 1893, 1894, 1895, 1896, 1994, 1995, 1996, 1997, 2099, 2095, 2096, 2097, 2098, 2197, 2198, 2199, 2200, 2299, 2300}; //Makes the river
             bool Disease = false;
             string[] forest = new string[10201];
-            int[] Age = new int[10201];
             for (int i = 0; i < 10201; i++)
             {
                 int Random = RandomNum(0, 4);
                 if (Random == 1)
                 {
                     forest[i] = "S";
-                    AGETREE(Age, forest, i);
-                    new NorwaySpruce(Doves(), River(i, RiverDisplay), BugHotel(Disease), Diseased(Disease), Age[i]);
+                    new NorwaySpruce(Doves(), River(i, RiverDisplay), BugHotel(Disease), Diseased(Disease), 0);
                 }
                 else
                 {
